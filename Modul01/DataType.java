912a6bfb9242a25f0344d3b6284180bb08590a71
M.ZidaYassir <m.zidayassir122@gmail.com>
2020-04-24 09:16:25 +0800

    deklarasi variabel dan tipe data

---
+++ Modul01/DataType.java
@@ -0,0 +1,12 @@
+public class DataType{
+ public static void main(String[]args){
+
+  String namaDepan ="Muhammad";
+  String namaTengah ="Zida";
+  String namaBelakang ="Yassir";
+  int usia =20;
+  int targetTahunKuliah = 4;
+  boolean tampan = true;
+
+ }
+}

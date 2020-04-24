130b9a5b241e32056bfcadf3e00dd310c6386f2e
M.ZidaYassir <m.zidayassir122@gmail.com>
2020-04-24 10:22:30 +0800

    Operasi Tipe String

--- Modul01/src/Main.java
+++ Modul01/src/Main.java
@@ -12,5 +12,21 @@ public class Main {
         System.out.println(uniskaString);
         System.out.println(namaDepan + namaTengah + namaBelakang);
 
+        String namaLengkap = namaDepan + " " +
+                namaTengah + " " + namaBelakang;
+        System.out.println(namaLengkap.length());
+        System.out.println(namaLengkap.indexOf("NIS"));
+        System.out.println(namaLengkap.substring(5));
+        System.out.println(namaLengkap.substring(5,12));
+        System.out.println(namaLengkap.replace( target: "Zi", replacement: "Barr"));
+        System.out.println(namaLengkap.toUpperCase());
+        System.out.println(namaLengkap.toLowerCase());
+        System.out.println(namaLengkap.charAt(6));
+
+        String[] namaArray = namaLengkap.split( regex: " ");
+
+        for(String nama : namaArray){
+            System.out.println(nama);
+        }
     }
 }

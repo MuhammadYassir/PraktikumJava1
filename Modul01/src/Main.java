b3949c647d156239045b488a97e4dea75b4c921b
M.ZidaYassir <m.zidayassir122@gmail.com>
2020-04-24 10:03:28 +0800

    Project IDEA

---
+++ Modul01/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        String namaDepan = "Muhammad";
+        String namaTengah = "Zida";
+        String namaBelakang = "Yassir";
+        int usia = 20;
+        int targetTahunKuliah = 4;
+        boolean tampan = true;
+
+        System.out.println(namaDepan + namaTengah + namaBelakang);
+    }
+}
